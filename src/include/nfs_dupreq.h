3f06a5ed5a4da3d5ca823c7e09ece65593ad7326
Jeremy Bongio <jbongio@us.ibm.com>
2010-11-08 11:00:10 -0500

    Combined current request check and duplicate request check in nfs_worker_thread.

--- src/include/nfs_dupreq.h
+++ src/include/nfs_dupreq.h
@@ -89,6 +89,9 @@ typedef struct dupreq_entry__
   struct sockaddr addr;
   int checksum;
 
+  pthread_mutex_t dupreq_mutex;
+  int processing; /* if currently being processed, this should be = 1 */
+
   nfs_res_t res_nfs;
   u_long rq_prog;               /* service program number        */
   u_long rq_vers;               /* service protocol version      */
@@ -104,12 +107,19 @@ int clean_entry_dupreq(LRU_entry_t * pentry, void *addparam);
 int nfs_dupreq_gc_function(LRU_entry_t * pentry, void *addparam);
 
 nfs_res_t nfs_dupreq_get(long xid, struct svc_req *ptr_req, SVCXPRT *xprt, int *pstatus);
+int nfs_dupreq_delete(long xid, struct svc_req *ptr_req, SVCXPRT *xprt,
+                      struct prealloc_pool *dupreq_pool);
+int nfs_dupreq_add_not_finished(long xid,
+				struct svc_req *ptr_req,
+				SVCXPRT *xprt,
+				struct prealloc_pool *dupreq_pool,
+				nfs_res_t *res_nfs);
 
-int nfs_dupreq_add(long xid,
-                   struct svc_req *ptr_req,
-		   SVCXPRT *xprt,
-                   nfs_res_t * p_res_nfs,
-                   LRU_list_t * lru_dupreq, struct prealloc_pool *dupreq_pool);
+int nfs_dupreq_finish(long xid,
+		      struct svc_req *ptr_req,
+		      SVCXPRT *xprt,
+		      nfs_res_t * p_res_nfs,
+		      LRU_list_t * lru_dupreq);
 
 unsigned long dupreq_value_hash_func(hash_parameter_t * p_hparam,
                                      hash_buffer_t * buffclef);
@@ -119,5 +129,7 @@ void nfs_dupreq_get_stats(hash_stat_t * phstat);
 #define DUPREQ_SUCCESS             0
 #define DUPREQ_INSERT_MALLOC_ERROR 1
 #define DUPREQ_NOT_FOUND           2
+#define DUPREQ_BEING_PROCESSED     3
+#define DUPREQ_ALREADY_EXISTS      4
 
 #endif                          /* _NFS_DUPREQ_H */
