c56e841ce4669ac15b36202555a14e2365e5cbcc
Jim Wahlig <jgwahlig@us.ibm.com>
2012-06-15 10:34:15 -0700

    few more refcount problems in V4 code.

--- src/Protocols/NFS/nfs4_op_secinfo.c
+++ src/Protocols/NFS/nfs4_op_secinfo.c
@@ -153,6 +153,7 @@ int nfs4_op_secinfo(struct nfs_argop4 *op,
       (secinfo4 *) Mem_Alloc(num_entry * sizeof(secinfo4))) == NULL)
     {
       res_SECINFO4.status = NFS4ERR_SERVERFAULT;
+      cache_inode_put(entry_src, data->pclient);
       return res_SECINFO4.status;
     }
 
@@ -185,6 +186,8 @@ int nfs4_op_secinfo(struct nfs_argop4 *op,
       res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_val[idx++].secinfo4_u.flavor_info.oid = v5oid;
     }
   res_SECINFO4.SECINFO4res_u.resok4.SECINFO4resok_len = idx;
+
+  cache_inode_put(entry_src, data->pclient);
 
   return res_SECINFO4.status;
 }                               /* nfs4_op_secinfo */
