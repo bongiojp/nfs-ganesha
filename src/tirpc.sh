32e8f1a140614752e69d1c16febd1f1aa2c75c1f
Matt Benjamin <matt@linuxbox.com>
2012-12-18 15:31:15 -0500

    Bump TI-RPC Version.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/tirpc.sh
+++ src/tirpc.sh
@@ -4,7 +4,7 @@ OPWD=`pwd`
 
 TIRPC_REPO='git://github.com/linuxbox2/ntirpc.git'
 TIRPC_BRANCH_NAME='duplex-7'
-TIRPC_COMMIT='04fd99484ebdba175c0f821f30bd8344082afcfe'
+TIRPC_COMMIT='65f13001a0e091df7e3dcbb22dba9300ac294646'
 
 # remove libtirpc if present;  try to avoid making
 # a mess
