70f46a3c33ee95ecf73048868bebd7833a20e0fa
suhrud <supatank@in.ibm.com>
2013-06-19 16:01:06 +0530

    GPFS specific init file. GPFS specific main.conf and exports.conf files. Signed-off-by: suhrud <supatank@in.ibm.com>

--- src/cmake/modules/UseRPMTools.cmake
+++ src/cmake/modules/UseRPMTools.cmake
@@ -348,6 +348,18 @@ install -m 644 scripts/systemd/nfs-ganesha.service       $RPM_BUILD_ROOT%{_unitd
 install -m 644 scripts/ganeshactl/org.ganesha.nfsd.conf  $RPM_BUILD_ROOT%{_sysconfdir}/dbus-1/system.d
 install -m 755 ganesha.sysconfig                         $RPM_BUILD_ROOT%{_sysconfdir}/sysconfig/ganesha
 install -m 755 tools/mount.9P				 $RPM_BUILD_ROOT%{_sbindir}/mount.9P
+")
+
+if(USE_FSAL_GPFS)
+FILE(APPEND ${RPM_ROOTDIR}/SPECS/${RPMNAME}.spec  "
+install -m 755 ganesha.gpfs.init                         $RPM_BUILD_ROOT%{_sysconfdir}/init.d/ganesha.gpfs
+install -m 644 config_samples/ganesha.conf               $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+install -m 644 config_samples/gpfs.ganesha.nfsd.conf     $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+install -m 644 config_samples/gpfs.ganesha.exports.conf  $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+install -m 644 config_samples/gpfs.ganesha.main.conf     $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
+")
+endif(USE_FSAL_GPFS)
+
 
 install -m 644 config_samples/ganesha.conf   $RPM_BUILD_ROOT%{_sysconfdir}/ganesha
 
@@ -369,6 +381,19 @@ rm -rf build_tree
 %dir %{_sysconfdir}/ganesha/
 %config(noreplace) %{_sysconfdir}/ganesha/ganesha.conf
 
+"
+)
+
+if(USE_FSAL_GPFS)
+FILE(APPEND ${RPM_ROOTDIR}/SPECS/${RPMNAME}.spec  "
+%config %{_sysconfdir}/init.d/ganesha.gpfs
+%config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.nfsd.conf
+%config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.exports.conf
+%config(noreplace) %{_sysconfdir}/ganesha/gpfs.ganesha.main.conf
+")
+endif(USE_FSAL_GPFS)
+
+FILE(APPEND ${RPM_ROOTDIR}/SPECS/${RPMNAME}.spec  "
 %files mount-9P
 %defattr(-,root,root,-)
 %{_sbindir}/mount.9P
