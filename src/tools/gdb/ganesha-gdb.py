aeb3641b5a6a5905e83e1f59c2e6d73de347bfaf
Jeremy Bongio <jbongio@us.ibm.com>
2013-07-23 11:34:38 -0400

    Convert print statements to print function. Fix bugs

--- src/tools/gdb/ganesha-gdb.py
+++ src/tools/gdb/ganesha-gdb.py
@@ -21,6 +21,7 @@ class Ganesha20:
     WRLOCK_FUNC = "pthread_rwlock_wrlock"
     RDLOCK_FUNC = "pthread_rwlock_rdlock"
 
+    WORKERTHREAD_FUNC = "worker_run"
 #CONFIG for 1.5
 class Ganesha15:
     CIREAD_FUNC = "cache_inode_rdwr"
@@ -36,13 +37,14 @@ class Ganesha15:
     WRLOCK_FUNC = "pthread_rwlock_wrlock"
     RDLOCK_FUNC = "pthread_rwlock_rdlock"
 
+    WORKERTHREAD_FUNC = "worker_run"
 class PrintLockOwners(gdb.Command):
     """print owner of locks
 
 Iterate through each thread that is blocked on a lock. Print
 inode entry info and lock and the lock owner."""
     def __init__(self):
-        self.ganeshafunc = getGaneshaConfig()
+        self.ganeshafunc = self.getGaneshaConfig()
         # This is where we register the new function with GDB
         super (PrintLockOwners, self).__init__ ("print_lock_owners",
                                                 gdb.COMMAND_SUPPORT,
@@ -56,7 +58,7 @@ inode entry info and lock and the lock owner."""
 
         if len(arg_list) < 1:
             # CI = Cache Inode
-            print "usage: print_lock_owners [CI_READ|CI_WRITE]"
+            print("usage: print_lock_owners [CI_RW|BACK]")
             return
         {'CI_RW': self.find_ci_rw_locks,
          'BACK': self.printbacktrace,
@@ -71,13 +73,14 @@ inode entry info and lock and the lock owner."""
         FILES_QUERY = "info files"
         
         # Let's see if we loaded a libfsal object file, which only exists in ganesha >2.0
-        lib_ptr = gdb.parse_and_eval(FILES_QUERY)
-        lib_str = "%s"%lib_ptr
-        result = re.search(r".*libfsal.*", lib_str, re.MULTILINE|re.IGNORECASE)
-        if self.isganesha20():
-            return Ganesha20()
-        else:
-            return Ganesha15()        
+        for obj in gdb.objfiles():
+            result = re.search(r".*libfsal.*", obj.filename, re.MULTILINE|re.IGNORECASE)
+            if result:
+                print("Looks like Ganesha 2.0, loading the appropriate config ...")
+                return Ganesha20()                
+        # We did not find an libfsal object file, must be Ganesha 1.5
+        print("Looks like Ganesha 1.5, loading the appropriate config ...")
+        return Ganesha15()        
     def parseAndEvaluate(self,exp):
         if gdb.VERSION.startswith("6.8.50.2009"):
             return gdb.parse_and_eval(exp)
@@ -89,14 +92,14 @@ inode entry info and lock and the lock owner."""
         return gdb.history(0)
     
     def printbacktrace(self):
-        print "Printing backtrace"
+        print("Printing backtrace")
         ganesha_process = gdb.inferiors()[0] # there should only be one inferior process
                                              # "inferior" refers to any process object
                                              # inferior to the GDB process.
         for thread in ganesha_process.threads():
-            print "Thread ", thread.num
+            print("Thread ", thread.num)
             if sys.version_info[0] == 2 and sys.version_info[2] > 4:
-                print " name:", thread.name
+                print(" name:", thread.name)
     
     
             thread.switch() # switch to this thread so we can explore further
@@ -114,17 +117,18 @@ inode entry info and lock and the lock owner."""
                     out += "at %s" % currsymtab.symtab.filename
                     out += ":%lu " % currsymtab.line
     
-                print out
+                print(out)
                 currframe = currframe.older()
-            print "\n"
+            print("\n")
         return    
     
     def find_ci_rw_locks(self):
-        print "Looking at threads waiting on Cache inode RW locks"
+        print("Looking at threads waiting on Cache inode RW locks")
         ganesha_process = gdb.inferiors()[0] # there should only be one inferior process
                                              # "inferior" refers to any process object
                                              # inferior to the GDB process.
         totalthreads = 0
+        totalworkerthreads = 0
         totalreading = 0
         totalwriting = 0
         totalwaitstoreadonread = 0
@@ -142,8 +146,8 @@ inode entry info and lock and the lock owner."""
                                     " = (\d+).*", re.MULTILINE|re.IGNORECASE)
     
         for thread in ganesha_process.threads():
-            if sys.version_info[0] == 2 and sys.version_info[2] > 4:
-                print "Thread name: ", thread.name
+#            if sys.version_info[0] == 2 and sys.version_info[2] > 4:
+#                print("Thread name: ", thread.name)
             #print "Thread  ", thread.num
     
             thread.switch() # switch to this thread so we can explore further
@@ -153,14 +157,14 @@ inode entry info and lock and the lock owner."""
             lockptr = None
             entryhandle = None
     
-            while currframe != None:                
+            while currframe != None:
                 if currframe.name() == self.ganeshafunc.FSALREAD_FUNC:
                     currframe = currframe.older()
                     while currframe != None:
                         if currframe.name() == self.ganeshafunc.CIREAD_FUNC:
                             # Make this frame the current frame in GDB
                             currframe.select()
-                            lockptr = gdb.parse_and_eval(self.ganeshafunc.self.ganeshafunc.CIREAD_CONTENTLOCK_REF)
+                            lockptr = gdb.parse_and_eval(self.ganeshafunc.CIREAD_CONTENTLOCK_REF)
                             lockptr_str = "%s"%lockptr
                             if not dictofowners.has_key(lockptr_str):
                                 dictofowners[lockptr_str] = [thread.num]
@@ -168,7 +172,7 @@ inode entry info and lock and the lock owner."""
                                 dictofowners[lockptr_str].append(thread.num)
                         currframe = currframe.older()
                 if currframe != None and currframe.name() == self.ganeshafunc.FSALWRITE_FUNC:
-                    print ""
+                    print("")
     
                 if currframe != None and (currframe.name() == self.ganeshafunc.WRLOCK_FUNC
                                           or currframe.name() == self.ganeshafunc.RDLOCK_FUNC):
@@ -184,7 +188,7 @@ inode entry info and lock and the lock owner."""
                             currframe.select()
     
                             # This has data on queued readers/writers, total readers, etc.
-                            lockptr = gdb.parse_and_eval(self.ganeshafunc.self.ganeshafunc.CIREAD_CONTENTLOCK_REF)
+                            lockptr = gdb.parse_and_eval(self.ganeshafunc.CIREAD_CONTENTLOCK_REF)
     
                             lockptr_str = "%s"%lockptr
                             if not dictoflocks.has_key(lockptr_str):
@@ -217,6 +221,8 @@ inode entry info and lock and the lock owner."""
                             #print "Found the line where we're waiting to WRITE"
                             totalwaitstowrite += 1
                             totalwriting += 1
+                        if currframe != None and currframe.name() == self.ganeshafunc.WORKERTHREAD_FUNC:
+                            totalworkerthreads += 1
                         currframe = currframe.older()
                         
                 if currframe != None and currframe.name() == self.ganeshafunc.NFSPROTO_READ_FUNC:
@@ -227,38 +233,42 @@ inode entry info and lock and the lock owner."""
                 if currframe != None and currframe.name() == self.ganeshafunc.NFSPROTO_WRITE_FUNC:
                     #print "Found the line where we're writing but not waiting"
                     totalwriting += 1
+
+                if currframe != None and currframe.name() == self.ganeshafunc.WORKERTHREAD_FUNC:
+                    totalworkerthreads += 1
     
                     # Find which lock this owns
                 if currframe != None:
                     currframe = currframe.older()
         
-        print "total threads: ", totalthreads
-        print "total waiting to read with a readlock: ", totalwaitstoreadonread
-        print "total waiting to read with a writelock: ", totalwaitstoreadonwrite
-        print "total waiting to write: ", totalwaitstowrite
-        print "total threads reading: ", totalreading
-        print "total threads writing: ", totalwriting
-        print "total num of locks: ", len(dictoflocks.keys()) # how many locks is the contention over?
+        print("total threads: ", totalthreads)
+        print("total worker threads: ", totalworkerthreads)
+        print("total waiting to read with a readlock: ", totalwaitstoreadonread)
+        print("total waiting to read with a writelock: ", totalwaitstoreadonwrite)
+        print("total waiting to write: ", totalwaitstowrite)
+        print("total threads reading: ", totalreading)
+        print("total threads writing: ", totalwriting)
+        print("total num of locks: ", len(dictoflocks.keys())) # how many locks is the contention over?
         for lock_str in dictoflocks.keys():
-            print "threads waiting on lock ", lock_str, ": ", dictoflocks[lock_str]
+            print("threads waiting on lock ", lock_str, ": ", dictoflocks[lock_str])
             (nr_readers, readers_wakeup, writers_wakeup,
              nr_readers_queued, nr_writers_queued,
              writer, shared) = dictoflockdata[lock_str]
-            print "\tnr_readers: ",nr_readers
-            print "\tnr_readers_wakeup: ",readers_wakeup
-            print "\twriters_wakeup: ",writers_wakeup
-            print "\tnr_readers_queued: ",nr_readers_queued
-            print "\tnr_writers_queued: ",nr_writers_queued
-            print "\twriter: ",writer
-            print "\tshared: ",shared
+            print("    nr_readers: ",nr_readers)
+            print("    nr_readers_wakeup: ",readers_wakeup)
+            print("    writers_wakeup: ",writers_wakeup)
+            print("    nr_readers_queued: ",nr_readers_queued)
+            print("    nr_writers_queued: ",nr_writers_queued)
+            print("    writer: ",writer)
+            print("    shared: ",shared)
         for owned_lock in dictofowners.keys():
-            print "Lock ", owned_lock, " owned by:"
+            print("Lock ", owned_lock, " owned by:")
             for threadnum in dictofowners[owned_lock]:
-                print "\tthread ", threadnum
+                print("    thread ", threadnum)
         return
     
     def functionnotfound(self):
-        print "Invalid argument. Supported args are CI_RW or BACK."
+        print("Invalid argument. Supported args are CI_RW or BACK.")
         return
 
 # Now register the commands:
