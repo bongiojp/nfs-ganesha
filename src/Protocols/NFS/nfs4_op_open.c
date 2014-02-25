01d92ac83beb8bdbbb8fc075d5567772cb00aae6
Jeremy Bongio <jbongio@us.ibm.com>
2014-02-25 18:00:07 -0500

    fixes segfault, need to look into further later..

--- src/Protocols/NFS/nfs4_op_open.c
+++ src/Protocols/NFS/nfs4_op_open.c
@@ -1312,8 +1312,7 @@ int nfs4_op_open(struct nfs_argop4 *op, compound_data_t *data,
 
 			PTHREAD_RWLOCK_unlock(&entry_lookup->state_lock);
 
-
-			if (entry_lookup == NULL) {
+			if (entry_lookup == NULL || file_state == NULL) {
 				res_OPEN4->status = nfs4_Errno(cache_status);
 				//res_OPEN4->status = NFS4ERR_DIRDELEG_UNAVAIL;
 				goto out;
