a1380a9ab4f055bd4da1f88dc0b5049686b5806a
Srikrishan Malik <srimalik@in.ibm.com>
2014-04-03 21:20:21 +0530

    Delegation: Add support to detect and handle open-recall race.
    Implements Section 10.4.5 of RFC 3530bis 25.
    If the first async recall fails with NFS4ERR_BADHANDLE, then the server
    synchronously retries for Deleg_recall_race_retry_count(config var)
    times with a delay of Deleg_recall_retry_delay(config var).
    
    After a successfull recall, the server will wait for one
    lease_lifetime before revoking the delegation if the client has
    not returned it.
    
    If the recall was not sucessfull the server will wait for two
    lease_lifetimes to give enough time to the client to reestablish
    a failed callback channel after NFS4ERR_CB_PATHDOWN.
    
    Signed-off-by: Srikrishan Malik <srimalik@in.ibm.com>

--- src/Protocols/NFS/nfs4_op_open.c
+++ src/Protocols/NFS/nfs4_op_open.c
@@ -946,7 +946,6 @@ static void get_delegation(compound_data_t *data, struct nfs_argop4 *op,
 			       &new_state->state_export_list);
 		pthread_mutex_unlock(&data->export->exp_state_mutex);
 
-
 		/* PTHREAD_RWLOCK_unlock(&data->current_entry->state_lock); */
 		state_status = state_lock(data->current_entry,
 					  data->export,
@@ -975,7 +974,7 @@ static void get_delegation(compound_data_t *data, struct nfs_argop4 *op,
 					space_limit.limitby = NFS_LIMIT_SIZE;
 				writeres->
 				space_limit.nfs_space_limit4_u.filesize =
-									100000;
+						DELEG_SPACE_LIMIT_FILESZ;
 				writeres->stateid =
 						saved_data->deleg.sd_stateid;
 				writeres->recall = FALSE;
