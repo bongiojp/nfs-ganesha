f81c4a9f9ea8cd53b522e83568dfb35882c23425
Srikrishan Malik <srimalik@in.ibm.com>
2014-04-23 17:43:49 +0530

    Added config vars for delegation recalls.
    deleg_recall_retry_count: Number of times the server to try to recall
    before revoking. default:3
    deleg_recall_retry_delay: Number of seconds the server will wait before
    retrying a recall.
    Signed-off-by: Srikrishan Malik <srimalik@in.ibm.com>

--- src/include/gsh_config.h
+++ src/include/gsh_config.h
@@ -398,11 +398,6 @@ typedef struct nfs_core_param {
 #define DELEG_RECALL_RETRY_COUNT_DEFAULT 3
 
 /**
- * @brief Default value of data deleg_recall_race_retry_count.
- */
-#define DELEG_RECALL_RACE_RETRY_COUNT_DEFAULT 3
-
-/**
  * @brief Default value of deleg_recall_retry_delay.
  */
 #define DELEG_RECALL_RETRY_DELAY_DEFAULT 1
@@ -436,13 +431,8 @@ typedef struct nfs_version4_parameter {
 	    group identifiers.  Defaults to true and is settable with
 	    Allow_Numeric_Owners. */
 	bool allow_numeric_owners;
-	/** Number of times the server will retry a recall before
-	    revoking the delegation */
+	/** Number of times the server will retry a recall */
 	uint32_t deleg_recall_retry_count;
-	/** Number of times the server will retry a recall before
-	    revoking the delegation when it detects a open-recall
-	    race. i.e client returns NFS4ERR_BADHANDLE for recall*/
-	uint32_t deleg_recall_race_retry_count;
 	/** Delay after which server will retry a recall in case of failures */
 	uint32_t deleg_recall_retry_delay;
 } nfs_version4_parameter_t;
