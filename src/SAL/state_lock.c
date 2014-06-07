42f763477263d8176d695f615313de0a344d9e31
Srikrishan Malik <srimalik@in.ibm.com>
2014-06-07 22:13:20 +0530

    state lock not required in subtract_deleg_from_list.
    Signed-off-by: Srikrishan Malik <srimalik@in.ibm.com>

--- src/SAL/state_lock.c
+++ src/SAL/state_lock.c
@@ -1041,7 +1041,6 @@ static state_status_t subtract_deleg_from_list(cache_entry_t *entry,
 	*removed = false;
 	glist_init(&remove_list);
 
-	PTHREAD_RWLOCK_wrlock(&entry->state_lock);
 	glist_for_each_safe(glist, glistn, list) {
 		found_entry = glist_entry(glist, state_lock_entry_t, sle_list);
 		if (owner != NULL
@@ -1068,7 +1067,6 @@ static state_status_t subtract_deleg_from_list(cache_entry_t *entry,
 		/* Then free the lock entry */
 		lock_entry_dec_ref(found_entry);
 	}
-	PTHREAD_RWLOCK_unlock(&entry->state_lock);
 	return status;
 }
 
